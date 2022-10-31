0736bad5b1db56d2d5af68f046b31830ec6fb9a9
noliprez <112441853+noliprez@users.noreply.github.com>
2022-10-31 19:43:15 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,32 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+#from app.forms import AddForm, DeleteForm, SearchForm, LoginForm, ChangePasswordForm
+#from app import db
+#from app.models import User
+import sys
 
 @app.route('/')
 def test():
     return render_template('test.html')
+
+@app.route('/login')
+def login():
+    #check if user is already logged in
+    #will redirect to homepage
+    if current_user.is_authenticated:
+        return redirect(url_for('/'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        # check DB for user by username
+        # have to create user
+        user = db.session.query(User).filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            print('Login failed', file=sys.stderr)
+            return redirect(url_for('login'))
+        # login_user is a flask_login function that starts a session
+        login_user(user)
+        print('Login successful', file=sys.stderr)
+        return redirect(url_for('view'))
+    return render_template('login.html', form=form)
