05d81b318fed57ef09d5c065f9075135efe4025c
Noliel Perez <nolielperez@gmail.com>
2022-11-09 16:01:28 +0000

    added missing import

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, StringField, PasswordField
+from wtforms import StringField, IntegerField, SubmitField, StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
