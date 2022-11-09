fceba9c392e428bb59394a2ffd91dba6038ba069
gzaigirdar <90423708+gzaigirdar@users.noreply.github.com>
2022-11-09 15:33:38 -0500

    "updated workplace form"

--- app/forms.py
+++ app/forms.py
@@ -18,11 +18,9 @@ class RegisterForm(FlaskForm):
 
     
 class WorkPlanForm(FlaskForm):
-    username = StringField("User's name: ", validators = [DataRequired()])
-    goal_description = StringField("Goal Description: ", validators = [DataRequired()])
-    submission_time = IntegerField('Time of Submission: ', validators = [DataRequired()])
-    project_description = StringField('Project Description', validators = [DataRequired()])
+    goal_description = TextAreaField("Goal Description: ", validators = [DataRequired()])
+    project_description = TextAreaField('Project Description', validators = [DataRequired()])
     project_members = StringField('Team Members: ', validators = [DataRequired()])
     current_goals = StringField('Current goals: ', validators = [DataRequired()])
-    completed_goals = BooleanField('Completed Goals', validators = [DataRequired()])
-    submit = SubmitField("Submit")
+    nextphase_goals = TextAreaField('Next phasegoals', validators = [DataRequired()])
+    submit = SubmitField("Add a Workplan")
