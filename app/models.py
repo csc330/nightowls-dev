5fc96c9fa5947769d7fe3d7542c3aed7d552e41a
Noliel Perez <nolielperez@gmail.com>
2022-11-09 19:38:15 +0000

    added some of the group functions

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
@@ -21,3 +22,8 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return db.session.query(User).get(int(id))
+
+class Groups(db.Model):
+    __tablename__ = 'groups'
+    id = db.Column(db.Integer, primary_key=True)
+    groupName = db.Column(db.String(64), unique=True)
