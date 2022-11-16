8f8a5d59189f610b50831d7bbba00011f5c6f997
Quaneysha Poindexter <qspoindexter@gmail.com>
2022-11-16 17:15:50 +0000

    database updates

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
@@ -15,6 +16,22 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Workplan(db.Models)
+    __tablename__ = 'workplan'
+    id = db.Column(db.Integer, foreign_key=True)
+    goal_description = db.Column(db.String(64), unique=True)
+    project_description = db.Column(db.String(64), primary_key=True, unique=True)
+    current_goals = db.Column(db.String(64), unique=True)
+    completed_goals = db.Column(db.Boolean, unique=True)
+
+class Evaluation(db.Models)
+    __tablename__ = 'evalutions'
+    id = db.Column(db.Integer, foreign_key=True)
+    role_description = db.Column(db.String(64), primary_key=True,  unique=True)
+    submission_history =db.Column(db.Integer, unique=True)
+    add_review = db.Column(db.String(64), unique=True)
+    add_rating = db.Column(db.String(64), unique=True)
 
 # load_user is a function that's used by flask_login to manage the session.
 # It simply returns the object associated with the authenticated user.
