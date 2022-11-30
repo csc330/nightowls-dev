6d20a74f2807b796783a169ede8ca40a20555aae
noliprez <112441853+noliprez@users.noreply.github.com>
2022-11-30 11:00:13 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 # New imports
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 from dotenv import load_dotenv
 from os import environ
